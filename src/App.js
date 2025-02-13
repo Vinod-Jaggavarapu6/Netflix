e629bef17c9eb1a3ca1bb549f3f46b8299ac22df
Vinod Jaggavarapu <jaggavarapu.sa@northeastern.edu>
2025-02-13 01:05:09 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-400">
+      Namaste Everyone, let's build Netflix.
+    </div>
+  );
+}
+
+export default App;
