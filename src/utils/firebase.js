9d580f37b779c4e2ef03192fbdc809f46f18089b
Vinod Jaggavarapu <jaggavarapu.sa@northeastern.edu>
2025-02-13 21:11:12 -0500

    env variables firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,18 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCE0ozHjHiPG5FeNRCXpM9J8uptjs3eadM",
-  authDomain: "netflixgpt-1753d.firebaseapp.com",
-  projectId: "netflixgpt-1753d",
-  storageBucket: "netflixgpt-1753d.firebasestorage.app",
-  messagingSenderId: "479775335332",
-  appId: "1:479775335332:web:eb8d89bf565b95b4769731",
-  measurementId: "G-LWK08W44MC",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
